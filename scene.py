3254d9c57a4af27c63025a42ea6dc3da1f937e3a
Vr3n <vickyspatel@gmail.com>
2024-01-24 23:55:01 +0530

    Scene

--- scene.py
+++ scene.py
@@ -13,7 +13,7 @@ class Scene:
 		app = self.app
 		add = self.add_object
 
-		n, s = 30, 3
+		n, s = 80, 2
 		for x in range(-n, n, s):
 			for z in range(-n, n, s):
 				add(Cube(app, pos=(x, -s, z)))
